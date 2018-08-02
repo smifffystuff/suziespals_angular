6b0ec6c99b952b48358793ddb768fd86340518f4
Martin Smith <martin.smifff@gmail.com>
2018-08-02 08:12:11 +0100

    Added skeleton components and filled out NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ConfirmEmailComponent } from './auth/confirm-email/confirm-email.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostComponent } from './posts/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    SignupComponent,
+    SigninComponent,
+    ConfirmEmailComponent,
+    ProfilesComponent,
+    PostsComponent,
+    PostsListComponent,
+    PostCreateComponent,
+    PostComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
